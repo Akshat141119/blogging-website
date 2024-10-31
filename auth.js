c477c954698afb1ef13a14e8234788d21c661d93
Akshat141119 <akshatrawat095@gmail.com>
2024-10-31 12:10:56 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -3,9 +3,9 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 // Configure the Google strategy for use by Passport.
 passport.use(new GoogleStrategy({
-    clientID: 'YOUR_GOOGLE_CLIENT_ID',
-    clientSecret: 'YOUR_GOOGLE_CLIENT_SECRET',
-    callbackURL: "http://localhost:3000/auth/google/callback"
+    clientID: ('109925229902-t7a38r78b4rbj0pur83scnc3guqk1b21.apps.googleusercontent.com'),
+    clientSecret: ('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'),
+    callbackURL: "http://localhost:5500/auth/google/callback"
   },
   function(token, tokenSecret, profile, done) {
     // You can save the user info to the database here
@@ -18,5 +18,5 @@ passport.serializeUser(function(user, done) {
 });
 
 passport.deserializeUser(function(obj, done) {
-  done(null, obj);
+  done(null, obj)
 });
